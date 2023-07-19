9eb033fd884791a3d37a152d6b40b47509740bfb
jonathan briceno <brimanz2014@gmail.com>
2023-07-19 18:36:07 -0400

    adding components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarEmpleadoComponent } from './components/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './components/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './components/listar-empleado/listar-empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent
   ],
   imports: [
     BrowserModule,
